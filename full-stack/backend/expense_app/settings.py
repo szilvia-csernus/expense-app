baa70d528c56faae22782ede7d9836d7518e6cfc
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-01-17 14:07:48 +0000

    replace python-decouple with python-dotenv

--- full-stack/backend/expense_app/settings.py
+++ full-stack/backend/expense_app/settings.py
@@ -1,31 +1,30 @@
 from pathlib import Path
-from decouple import Config
+from dotenv import load_dotenv
 import os
-
-# Get the path to the directory of the current file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Get the current environment from the DJANGO_ENV variable
-django_env = os.getenv('DJANGO_ENV', 'development')
-
-# Load the appropriate .env file
-env_file = os.path.join(current_dir, f'.env.{django_env}')
-config = Config(env_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Check if we're in development mode
+if os.getenv('DEVELOPMENT'):
+    # Load the development .env file
+    load_dotenv(os.path.join(BASE_DIR, '.env.dev'))
+else:
+    # Load the production .env file
+    load_dotenv(os.path.join(BASE_DIR, '.env.prod'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", cast=str)
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",
@@ -44,20 +43,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage', # for cloudinary
+    'cloudinary_storage',  # for cloudinary
     'django.contrib.staticfiles',
-    'cloudinary', # for cloudinary
+    'cloudinary',  # for cloudinary
     'rest_framework',
-    'corsheaders', # new
-    
+    'corsheaders',  # new
+
     'custom_commands',
     'profiles',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', # new
+    'corsheaders.middleware.CorsMiddleware',  # new
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # new
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # new
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,41 +89,47 @@ WSGI_APPLICATION = 'expense_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
+# if 'DEVELOPMENT' in os.environ:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("POSTGRES_DB", cast=str),
-        'USER': config("POSTGRES_USER", cast=str),
-        'PASSWORD': config("POSTGRES_PASSWORD", cast=str),
-        'HOST': 'db', # set in docker-compose.yml
-        'PORT': '5432' # default postgres port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# else:
+
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.getenv("POSTGRES_DB"),
+#             'USER': os.getenv("POSTGRES_USER"),
+#             'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+#             'HOST': 'db',  # set in docker-compose.prod.yml
+#             'PORT': '5432'  # default postgres port
+#         }
+#     }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+ {
+  'NAME':
+  'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+ },
+ {
+  'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+ },
+ {
+  'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+ },
+ {
+  'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+ },
 ]
 
 
@@ -147,8 +152,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles' # new
-WHITENOISE_STATIC_ROOT = BASE_DIR / 'staticfiles' # new
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # new
+WHITENOISE_STATIC_ROOT = BASE_DIR / 'staticfiles'  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -156,7 +161,7 @@ WHITENOISE_STATIC_ROOT = BASE_DIR / 'staticfiles' # new
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CLOUDINARY_STORAGE = {
-    'CLOUDINARY_URL': config("CLOUDINARY_URL", cast=str),
+    'CLOUDINARY_URL': os.getenv("CLOUDINARY_URL"),
 }
 
 MEDIA_URL = '/media/'
--- full-stack/backend/profiles/serializers.py
+++ full-stack/backend/profiles/serializers.py
@@ -4,7 +4,7 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+
     class Meta:
         model = Profile
         fields = [
