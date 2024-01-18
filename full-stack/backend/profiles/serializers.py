1f26569d8e8c7513c6beaaf55d7676c7c97498cc
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-01-18 09:33:46 +0000

    add permissions to profiles

--- full-stack/backend/profiles/serializers.py
+++ full-stack/backend/profiles/serializers.py
@@ -4,10 +4,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        return obj.owner == self.context['request'].user
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'name', 'created_at', 'updated_at',
-            'bank_account', 'bank_account_name'
+            'bank_account', 'bank_account_name', 'is_owner'
         ]
--- full-stack/backend/profiles/views.py
+++ full-stack/backend/profiles/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProfileSerializer
+from .permissions import IsOwnerOrReadOnly
 
 from .models import Profile
 
@@ -10,28 +11,34 @@ from .models import Profile
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(profiles, many=True,
+                                       context={'request': request})
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
     # This is for the django browsable API to display the fields
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, request, pk):
         try:
-            return Profile.objects.get(pk=pk)
+            profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(request, profile)
+            return profile
+
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(request, pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(request, pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data,
+                                       context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -45,7 +52,8 @@ class ProfileDetail(APIView):
     def patch(self, request, pk):
         profile = self.get_object(request, pk)
         serializer = ProfileSerializer(profile, data=request.data,
-                                       partial=True)
+                                       partial=True,
+                                       context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
