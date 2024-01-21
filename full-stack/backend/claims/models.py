2b4bb20fa0d6d61e6b893263f663db0eba023503
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-01-21 11:50:03 +0000

    edit claims counter admin

--- full-stack/backend/claims/models.py
+++ full-stack/backend/claims/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class ClaimsCounter(models.Model):
     counter = models.IntegerField()
 
+    class Meta:
+        verbose_name_plural = "Counter"
+
     def increment(self):
         self.counter += 1
         self.save()
