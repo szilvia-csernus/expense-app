f155853ca3f071500342117e88da8d34654c2153
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-01-23 22:22:59 +0000

    add props type

--- full-stack/frontend/src/Components/Container.tsx
+++ full-stack/frontend/src/Components/Container.tsx
@@ -1,6 +1,11 @@
 import classes from './Container.module.css';
 
-export const Container = (props) => {
+interface ContainerProps {
+	className?: string;
+	children: React.ReactNode;
+}
+
+export const Container = (props: ContainerProps) => {
 	const classNames = `${classes.container} ${props.className ? props.className : ""}`;
 	return <div className={classNames}>{props.children}</div>;
 };
