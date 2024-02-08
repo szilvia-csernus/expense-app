114605207d99acc3013bb2ccd473f106e6d77f22
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-08 18:03:44 +0000

    update gunicorn settings: worker number and timeout

--- full-stack/backend/entrypoint.sh
+++ full-stack/backend/entrypoint.sh
@@ -14,4 +14,4 @@ python manage.py migrate --no-input
 python manage.py superuser
 python manage.py collectstatic --no-input
 
-gunicorn expense_app.wsgi:application --bind "0.0.0.0:8000"
+gunicorn expense_app.wsgi:application --bind "0.0.0.0:8000" -w 4 -t 60
