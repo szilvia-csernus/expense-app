cab1db6c4701cde6a7a092271ed6a0a70142dac8
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-09 14:36:03 +0000

    refactor send_expense_form

--- full-stack/backend/claims/forms.py
+++ full-stack/backend/claims/forms.py
@@ -11,22 +11,20 @@ class ExpenseForm(forms.Form):
     description = forms.CharField(max_length=500)
     total = forms.DecimalField(max_digits=10, decimal_places=2)
     iban = forms.CharField(max_length=34)
-    accountName = forms.CharField(max_length=200)
-    receipts = forms.ImageField(required=True)
+    accountName = forms.CharField(max_length=200, required=False)
 
 
-class ImageUploads(forms.Form):
-    receipts = forms.ImageField(required=True)
+class ReceiptUploads(forms.Form):
+    receipt = forms.FileField(required=False)
 
-    # This method is used to validate the image file type, overriding the
-    # default clean method to include PDF files.
-    def clean_image(self):
-        image = self.cleaned_data.get('image')
-        if image:
+    # This method is used to validate uploaded receipts' file types
+    def clean_receipt(self):
+        receipt = self.cleaned_data.get('receipt')
+        if receipt:
             # content_type is the file's MIME type, determined by the browser
-            main, sub = image.content_type.split('/')
+            main, sub = receipt.content_type.split('/')
             if not (main == 'image' and sub in ['jpeg', 'png']) and \
                     not (main == 'application' and sub == 'pdf'):
                 raise ValidationError('Unsupported file type. Supported file \
                     types are .jpg, .jpeg, .png, .pdf')
-        return image
+        return receipt
--- full-stack/backend/claims/views.py
+++ full-stack/backend/claims/views.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 from xhtml2pdf import pisa
 from django.shortcuts import get_object_or_404
 from .models import ClaimsCounter
-from .forms import ExpenseForm, ImageUploads
+from .forms import ExpenseForm, ReceiptUploads
 from churches.models import Church
 import os
 
@@ -26,7 +26,7 @@ def validate_form(request):
     if not form.is_valid():
         return Response(status=400, data=form.errors)
 
-    receipts = [ImageUploads({'image': request.FILES.get(name)})
+    receipts = [ReceiptUploads({'receipt': request.FILES.get(name)})
                 for name in request.FILES
                 if name.startswith('receipt')]
 
@@ -36,11 +36,72 @@ def validate_form(request):
     return None
 
 
+def update_form_with_additional_data(request, counter):
+    """
+    Append additional data to the form, such as the counter, the church's name,
+    and the logo's URL.
+    """
+    form = request.data
+
+    form['counter'] = counter
+    church_name = form['church']
+    logo = get_object_or_404(Church, name=church_name).logo
+    form['logo'] = logo.url
+    return form
+
+
+def generate_main_message(form):
+    """
+    Generate the main message for the email, using the form data.
+    """
+    return (
+        f"Name: {form['name']}\n"
+        f"Email: {form['email']}\n"
+        f"Church: {form['church']}\n"
+        f"Purpose: {form['purpose']}\n"
+        f"Date of Expense: {form['date']}\n"
+        f"Description: {form['description']}\n"
+        f"Total: {form['total']}\n"
+        f"Bank account: {form['iban']}\n"
+        f"Name of Bank Account Holder: {form['accountName']}\n\n"
+    )
+
+
+def generate_message_to_submitter(form, main_message):
+    """
+    Generate the message to the submitter, using the form data.
+    """
+    return (
+        f"Dear {form['name']}, \n\n"
+        f"Thank you for submitting an expense form. "
+        f"We will process it shortly.\n"
+        f"If you don't hear from us, or if the reimbursement doesn't arrive "
+        f"to you within 2 weeks, then please reach out to us at "
+        f"{os.environ['FINANCE_EMAIL']}.\n\n"
+        f"Attila\n"
+        f"Finance Team\n"
+        f"Redeemer International Church Rotterdam\n\n"
+        f"Ps - the submitted data:\n\n" +
+        main_message
+    )
+
+
+def generate_message_to_finance(form, main_message):
+    """
+    Generate the message to the finance team, using the form data.
+    """
+    return (
+        "Expense Form Submission\n\n" +
+        main_message
+    )
+
+
 def process_file(file, max_size):
     """
     Open the file, if it's an image, resize if necessary and convert it to PDF.
     Return a buffer with the PDF data.
     """
+    # Create a file-like buffer for the form to receive PDF data.
     buffer = BytesIO()
 
     if file.content_type.startswith('image/'):
@@ -65,59 +126,17 @@ def process_file(file, max_size):
     return buffer
 
 
-@api_view(['POST'])
-def send_expense_form(request):
-    validate_form(request)
-    counter_obj = get_object_or_404(ClaimsCounter, pk=1)
-    counter = str(counter_obj.counter)
-    form = request.data
-
-    form['counter'] = counter
-    church_name = form['church']
-    logo = get_object_or_404(Church, name=church_name).logo
-    form['logo'] = logo.url
-
-    main_message = (
-        'Name: ' + form['name'] + '\n' +
-        'Email: ' + form['email'] + '\n' +
-        'Church: ' + form['church'] + '\n' +
-        'Purpose: ' + form['purpose'] + '\n' +
-        'Date of Expense: ' + form['date'] + '\n' +
-        'Description: ' + form['description'] + '\n' +
-        'Total: ' + form['total'] + '\n' +
-        'Bank account: ' + form['iban'] + '\n' +
-        'Name of Bank Account Holder: ' + form['accountName'] + '\n\n'
-    )
-
-    message_to_submitter = (
-        'Dear ' + form['name'] + ', \n\n' +
-        'Thank you for submitting an expense form. We will process ' +
-        'it shortly.' + '\n' +
-        "If you don't hear from us, or if the reimbursement doesn't arrive " +
-        'to you within 2 weeks, then please reach out to us at ' +
-        os.environ['FINANCE_EMAIL'] + '.\n\n' +
-
-        'Attila' + '\n' +
-        'Finance Team' + '\n' +
-        'Redeemer International Church Rotterdam' + '\n\n' +
-
-        'Ps - the submitted data:' + '\n\n' +
-        main_message
-    )
-
-    message_to_finance = (
-        'Expense Form Submission' + '\n\n' +
-        main_message
-    )
-
-    # Create a file-like buffer to receive PDF data.
-    form_buffer = BytesIO()
-
-    # Generate the PDF from the form HTML and add the pdf to the buffer.
+def generate_form_pdf(form):
+    """
+    Generate HTML from the form data, convert it to PDF and add it to the
+    buffer. Return the buffer.
+    """
+    # Create a file-like buffer for the form to receive PDF data.
+    buffer = BytesIO()
     try:
         html_content = render_to_string('email-template.html', form)
-        pisa.CreatePDF(html_content, dest=form_buffer)
-        form_buffer.seek(0)
+        pisa.CreatePDF(html_content, dest=buffer)
+        buffer.seek(0)
     except Exception as e:
         print(f"Error generating PDF: {e}")
         return Response(status=500, data={"message": "Unfortunately, we were \
@@ -125,8 +144,21 @@ def send_expense_form(request):
                                           sent us. Please try taking a picture\
                                            of it instead."})
 
-    # The merger pdf will merge this and the upcoming PDF receipts together.
+    buffer.seek(0)
+    return buffer
+
+
+def generate_attachement(form):
+    # The merger pdf will merge the generated PDFs together.
     pdf_merge = PdfWriter()
+
+    # Generate the PDF from the form and add the pdf to the form_buffer. If the
+    # form_buffer is a Response, meaning there was an error, return this
+    # response.
+    form_buffer = generate_form_pdf(form)
+    if isinstance(form_buffer, Response):
+        return form_buffer
+
     pdf_merge.append(form_buffer)
     form_buffer.close()
 
@@ -141,13 +173,46 @@ def send_expense_form(request):
         try:
             max_size = (700, 1100)
             receipt_buffer = process_file(receipt_file, max_size)
-            pdf_merge.append(receipt_buffer)
-            receipt_buffer.close()
+            if receipt_buffer is not None:
+                pdf_merge.append(receipt_buffer)
+                receipt_buffer.close()
 
         except Exception as e:
             print(f"Error processing receipt {i}: {e}")
+            return Response(status=500, data={"message": "Unfortunately, we \
+                                          are unable to process one or more \
+                                          image(s) you sent us. Please \
+                                          try another format."})
 
         i += 1
+
+    return pdf_merge
+
+
+@api_view(['POST'])
+def send_expense_form(request):
+    # Validate the form and the images, and return a 400 response if invalid.
+    form_validation_response = validate_form(request)
+    if isinstance(form_validation_response, Response):
+        return form_validation_response
+
+    # Retrieve the counter from the database.
+    counter_obj = get_object_or_404(ClaimsCounter, pk=1)
+    counter = str(counter_obj.counter)
+
+    # Update the form with data retrieved from the database.
+    form = update_form_with_additional_data(request, counter)
+
+    # Construct the text messages for the emails.
+    main_message = generate_main_message(form)
+    message_to_submitter = generate_message_to_submitter(form, main_message)
+    message_to_finance = generate_message_to_finance(form, main_message)
+
+    # Construct the PDF attachment. If the attachment is a Response, meaning
+    # there was an error, return this response to the user.
+    attachement = generate_attachement(form)
+    if isinstance(attachement, Response):
+        return attachement
 
     # Create the email message.
     subject = 'Expense Form ' + counter + ' - ' + form['purpose']
@@ -166,22 +231,26 @@ def send_expense_form(request):
     )
 
     # Create the final PDF and close the merger.
-    final_buffer = BytesIO()
-    pdf_merge.write(final_buffer)
+    buffer_for_attachement = BytesIO()
+    attachement.write(buffer_for_attachement)
 
     # Attach the final PDF to the emails an send them.
     attachment_name = 'EF' + counter + '.pdf'
     email_to_finance.attach(attachment_name,
-                            final_buffer.getvalue(), "application/pdf")
+                            buffer_for_attachement.getvalue(),
+                            "application/pdf")
     email_to_finance.send()
     email_acknowledgement.attach(attachment_name,
-                                 final_buffer.getvalue(), "application/pdf")
+                                 buffer_for_attachement.getvalue(),
+                                 "application/pdf")
     email_acknowledgement.send()
 
-    final_buffer.close()
-    pdf_merge.close()
+    buffer_for_attachement.close()
+    attachement.close()
 
     # After the email has been sent, increment the counter
     counter_obj.increment()
 
+    print(f"Email sent for expense form {counter} to {form['email']} and \
+          {os.environ['FINANCE_EMAIL']}")
     return Response(status=200, data={"message": "Email sent."})
