a51c56915b17016925cb6a02627741ea4c843835
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-10 13:33:45 +0000

    adjust input fields' max_length

--- full-stack/backend/churches/models.py
+++ full-stack/backend/churches/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Church(models.Model):
     """Church model"""
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     logo = models.ImageField(upload_to='logos/', null=True, blank=True)
 
     def __str__(self):
@@ -19,7 +19,7 @@ class CostPurpose(models.Model):
                                null=True,
                                related_name='cost_purposes',
                                on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     cost_code = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
