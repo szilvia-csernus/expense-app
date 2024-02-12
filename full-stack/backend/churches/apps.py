100fcc3bc9f7e856baf765f83df07db28d59903a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-12 15:29:59 +0000

    remove redundant churches app

--- full-stack/backend/churches/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChurchesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'churches'
--- full-stack/backend/churches/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ChurchDetails, ChurchList
-
-urlpatterns = [
-    path('details/', ChurchDetails.as_view()),
-    path('names/', ChurchList.as_view()),
-]
