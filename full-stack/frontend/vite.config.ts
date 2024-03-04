3ad04b2dac2859ede178d924196739511e538a9f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-03-04 21:46:34 +0000

    update frontend for PWA

--- full-stack/frontend/vite.config.ts
+++ full-stack/frontend/vite.config.ts
@@ -1,20 +1,106 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dotenv from 'dotenv';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // Load environment variables
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
 	server: {
 		proxy: {
 			'/api': {
-			target: 'http://127.0.0.1:8000',
-			changeOrigin: true,
-			rewrite: (path) => path.replace(/^\/api/, '')
-			}
+				target: 'http://127.0.0.1:8000',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
 		},
 	},
+	plugins: [
+		react(),
+		VitePWA({
+			manifest: false,
+			strategies: 'generateSW',
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+			workbox: {
+				globPatterns: ['**/*.{js,jsx,ts,tsx,css,html,ico,png}'],
+				runtimeCaching: [
+					{
+						urlPattern: /^http:\/\/localhost/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'landing-page',
+							expiration: {
+								maxEntries: 1,
+								maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200],
+							},
+						},
+					},
+					{
+						urlPattern: /^https:\/\/res.cloudinary*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'cost-center-logos',
+							expiration: {
+								maxEntries: 1,
+								maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200],
+							},
+						},
+					},
+					{
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'google-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200],
+							},
+						},
+					},
+					{
+						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'gstatic-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200],
+							},
+						},
+					},
+					{
+						urlPattern: /^https:\/\/www\.redeemerdelft\.nl\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'logo-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200],
+							},
+						},
+					},
+				],
+			},
+		}),
+	],
 });
